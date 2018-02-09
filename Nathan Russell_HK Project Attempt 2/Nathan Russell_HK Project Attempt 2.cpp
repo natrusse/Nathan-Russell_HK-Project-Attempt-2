a2d24393f28698ba7cff67a976b13e4f47a0459c
NATHAN RUSSELL <natrusse@uat.edu>
2018-02-09 14:50:11 -0700

    Added Human Player; only replay loop is needed.

--- Nathan Russell_HK Project Attempt 2/Nathan Russell_HK Project Attempt 2.cpp
+++ Nathan Russell_HK Project Attempt 2/Nathan Russell_HK Project Attempt 2.cpp
@@ -1,6 +1,6 @@
-// Nathan Russell_HK Game Project-2.cpp : Defines the entry point for the console application.
+// Nathan Russell_HK Game Project-2.cpp
 // last edited 2/3/2018 at 10:28
-// not currently in github, update asap
+// last synced 
 
 #include "stdafx.h"
 
@@ -29,27 +29,79 @@ void persona() {
 
 int main()
 {
-	srand(static_cast<unsigned int>(time(0)));
 	//declaring variables
-	//todo add variables for other players 
+	srand(static_cast<unsigned int>(time(0)));
+	
+	//sets enemy location for code entirety
 	int enemylocation = rand() % 64 + 1;
+	//Player attempt variable
 	int binaryTries = 0;
 	int randomTries = 0;
 	int linearTries = 0;
+	int humanTries = 0;
+	//Player guess variable
 	int binaryGuess;
 	int randomGuess;
 	int linearGuess = 0;
+	int humanGuess;
+	//range answers must be within
 	int high = 64;
 	int low = 1;
+	//boolean logic to run accurate loops
 	bool isEnemyFoundbyBinarySearch = false;
 	bool isEnemyFoundbyrandomSearch = false;
 	bool isEnemyFoundbylinearSearch = false;
-	string test;
+	bool isEnemyFoundbyhumanSearch = false;
 
 	persona();
 
-	//todo add human search player
+	//human search player
+	while (isEnemyFoundbyhumanSearch == false) {
+		//starts game loop
 
+
+		//each guess resets the middle number between the high and low
+
+		cout << "Input Enemy location estimate: " << endl;
+		cin >> humanGuess;
+		++humanTries;
+
+		//output if guess is too high
+		if (humanGuess > enemylocation)
+		{
+			cout << "\tSearch grid " << humanGuess << " yielded no results. Lower guess recommended.\n\n";
+			//high decreases to compensate for guess
+			high = humanGuess--;
+
+		}
+		//output if guess is too low
+		else if (humanGuess < enemylocation)
+		{
+			cout << "\tSearch grid " << humanGuess << " yielded no results. Higher guess recommended.\n\n";
+			//low increases to compensate for guess
+			low = humanGuess++;
+
+		}
+		//output if the guess is the enemy location
+		else if (humanGuess == enemylocation)
+		{
+			cout << "\n\tEnemy Located in Grid #" << enemylocation << ". Enemy located in " << humanTries << " attempts!\n\n";
+			//ends the loop
+			isEnemyFoundbyhumanSearch = true;
+		}
+		//output if incorrect input is used
+		else
+		{
+			cout << "human search malfunction, please input within parameters " << high << " and" << low << "." << endl;
+			continue;
+		}
+
+
+
+		//human search code ends here
+	}
+	system("pause");
+	cout << "\n\tNow preparing for Linear AI Search.....\n\n" << endl;
 
 	//linear ai search player
 	while (isEnemyFoundbylinearSearch == false) {
@@ -188,6 +240,7 @@ int main()
 
 		//todo add final stats (list all player tries)
 
+	cout << "\n\tHuman Search effeciency rating: " << humanTries << " attempts.\n";
 	cout << "\n\tLinear Search effeciency rating: " << linearTries << " attempts.\n";
 	cout << "\n\tRandom Search effeciency rating: " << randomTries << " attempts.\n";
 	cout << "\n\tBinary Search effeciency rating: " << binaryTries << " attempts.\n\n";
