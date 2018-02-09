f3ab19a2f3e0f72a066897f472bfa6516eb932d4
NATHAN RUSSELL <natrusse@uat.edu>
2018-02-09 13:38:40 -0700

    Adds base AI and linear search players, heavy modifications required.

--- Nathan Russell_HK Project Attempt 2/Nathan Russell_HK Project Attempt 2.cpp
+++ Nathan Russell_HK Project Attempt 2/Nathan Russell_HK Project Attempt 2.cpp
@@ -31,16 +31,117 @@ int main()
 {
 	srand(static_cast<unsigned int>(time(0)));
 	//declaring variables
+	//todo add variables for other players 
 	int enemylocation = rand() % 64 + 1;
 	int binaryTries = 0;
+	int randomTries = 0;
+	int linearTries = 0;
 	int binaryGuess;
+	int randomGuess;
+	int linearGuess;
 	int high = 64;
 	int low = 1;
 	bool isEnemyFoundbyBinarySearch = false;
+	bool isEnemyFoundbyrandomSearch = false;
+	bool isEnemyFoundbylinearSearch = false;
 	string test;
 
 	persona();
 
+	//todo add human search player
+
+
+	//linear ai search player
+	while (isEnemyFoundbylinearSearch == false) {
+		//starts game loop
+
+
+		//each guess resets the middle number between the high and low
+		linearGuess = ((high - low) / 2) + low;
+		++linearTries;
+
+		//output if guess is too high
+		if (linearGuess > enemylocation)
+		{
+			cout << "\tSearch grid " << linearGuess << " yielded no results. Lower guess recommended because enemy location is " << enemylocation << ".\n\n";
+			//high decreases to compensate for guess
+			high = linearGuess--;
+
+		}
+		//output if guess is too low
+		else if (linearGuess < enemylocation)
+		{
+			cout << "\tSearch grid " << linearGuess << " yielded no results. Higher guess recommended because enemy location is " << enemylocation << ".\n\n";
+			//low increases to compensate for guess
+			low = linearGuess++;
+
+		}
+		//output if the guess is the enemy location
+		else if (linearGuess == enemylocation)
+		{
+			cout << "\n\tEnemy Located in Grid #" << enemylocation << ". Enemy located in " << linearTries << " attempts!\n";
+			cout << "Preparing to send agents to neutralize.......\n";
+			//ends the loop
+			isEnemyFoundbylinearSearch = true;
+		}
+		//output if incorrect input is used
+		else
+		{
+			cout << "linear search malfunction, please input within parameters " << high << " and" << low << "." << endl;
+			continue;
+		}
+
+
+
+		//linear search code ends here
+	}
+
+	//random ai search player
+	while (isEnemyFoundbyrandomSearch == false) {
+		//starts game loop
+
+
+		//each guess resets the middle number between the high and low
+		randomGuess = ((high - low) / 2) + low;
+		++randomTries;
+
+		//output if guess is too high
+		if (randomGuess > enemylocation)
+		{
+			cout << "\tSearch grid " << randomGuess << " yielded no results. Lower guess recommended because enemy location is " << enemylocation << ".\n\n";
+			//high decreases to compensate for guess
+			high = randomGuess--;
+
+		}
+		//output if guess is too low
+		else if (randomGuess < enemylocation)
+		{
+			cout << "\tSearch grid " << randomGuess << " yielded no results. Higher guess recommended because enemy location is " << enemylocation << ".\n\n";
+			//low increases to compensate for guess
+			low = randomGuess++;
+
+		}
+		//output if the guess is the enemy location
+		else if (randomGuess == enemylocation)
+		{
+			cout << "\n\tEnemy Located in Grid #" << enemylocation << ". Enemy located in " << randomTries << " attempts!\n";
+			cout << "Preparing to send agents to neutralize.......\n";
+			//ends the loop
+			isEnemyFoundbyrandomSearch = true;
+		}
+		//output if incorrect input is used
+		else
+		{
+			cout << "random search malfunction, please input within parameters " << high << " and" << low << "." << endl;
+			continue;
+		}
+
+
+
+		//random search code ends here
+	}
+
+	//binary search player
 	while (isEnemyFoundbyBinarySearch == false) {
 		//starts game loop
 
@@ -84,6 +185,11 @@ int main()
 
 		//binary search code ends here
 	}
+	
+	//todo add final stats (list all player tries)
+
+	//todo ask human player to play again
+
 	system("pause");
 	return 0;
 }
